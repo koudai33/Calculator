de18d4e110fe0ef75f2e156461ed9dc2930eb239
koudai <yamaokoudai33@gmail.com>
2020-03-24 10:36:53 +0900

    build project

--- lib/main.dart
+++ lib/main.dart
@@ -1,168 +1,240 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp( MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',//最近使ったアプリ一覧に表示される時の名前
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        //アプリのテーマカラーを決める
 
-      ),
-      home: MyHomePage(title: 'calculator'),
+  Widget build(BuildContext buildContext) {
+    return  new MaterialApp(
+      title: 'Calculator',//最近使ったアプリ一覧に表示される時の名前
+      home: new CloneCalc(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) :super (key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() =>  _MyHomePageState();
+class CloneCalc extends StatefulWidget {
+  CloneCalcState createState() =>  CloneCalcState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {//複雑な処理はこちらのStateクラスに記入する
+class CloneCalcState extends State<CloneCalc> {//複雑な処理はこちらのStateクラスに記入する
 //デフォルトの状態
-  String output = "0";
-  String _output = "0";
+  String text = '';
   double num1 = 0.0;
   double num2 = 0.0;
-  String operand = "";
+  String finalresult = '';
+  String result = '';
+  String opr = '';
+  String preopr = '';
 
-buttonPressed(String buttonText){//ボタンが押された時の処理
-  if(buttonText == "clear"){
-    _output = "0";
-    num1 = 0.0;
-    num2 = 0.0;
-    operand = "";
-  }else if(buttonText == "+" || buttonText == "-" || buttonText == "/" || buttonText == "X"){
-    num1 = double.parse(output);//parse()メソッドで文字列を浮動小数点
-    operand = buttonText;
-    _output = "0";
-  }else if(buttonText == "."){
-    if(_output.contains(".")){//与えられた要素がリストにあるか確認する
-      print( "already conataind a decimals");
-      return;
-    }else{
-      _output = _output + buttonText;
-    }
-  }else if(buttonText == "="){
-    num2 = double.parse(output);//parse()メソッドで文字列を浮動小数点に変換
-    if(operand == "+"){
-      _output = (num1 + num2).toString();
-    }
-    if(operand == "-"){
-      _output = (num1 - num2).toString();
-    }
-    if(operand == "X"){
-      _output = (num1 * num2).toString();
-    }
-    if(operand == "/"){
-      _output = (num1 / num2).toString();
-    }
+//bottonの形や余白
+Widget btn(btnText, Color color){
+  return Container(
+    padding: EdgeInsets.only(bottom: 10.0),
+    child: new RaisedButton(
+      child: Text(btnText,
+      style: TextStyle(
+        fontSize:30
+      ),),
+      onPressed: (){
+        calculation(btnText);
+      },
+      color: color,
+      padding: EdgeInsets.all(22.0),//周りの余白
+      shape: CircleBorder(),//丸ボタン
+    ),
+  );
+}
 
-    num1 = 0.0;
-    num2 = 0.0;
-    operand = "0";
-  }else {
-    _output = _output + buttonText;
-  }
- print(_output);
-
-
-  setState(() {
-    output = double.parse(_output).toStringAsFixed(2);//少数第何位まで表示するか
-  });
+//0ボタン
+Widget btnZero(btnText, Color color){
+  return Container(
+    padding: EdgeInsets.only( bottom: 10.0),//底だけ指定する
+    child: new RaisedButton(
+      child:Text( btnText,
+      style:TextStyle(
+        fontSize: 30
+      ),
+      ),
+      onPressed: (){
+        calculation(btnText);
+      },
+      color: color,
+      padding: EdgeInsets.only(left:81,top:20,right:81,bottom:20),
+      shape: StadiumBorder(),//横丸ボタン
+    ),
+  );
 }
 
 
-  Widget buildBoutton(String buttonText){
-    return new Expanded(
-      child: new RaisedButton(//ボタンの設定
-      color: Colors.white,
-        shape: CircleBorder(
-          side: BorderSide(
-          color: Colors.black,
-          width: 1.0,
-          style: BorderStyle.solid,)),
-        padding: new EdgeInsets.all(30.0),
-         child: new Text(buttonText,
-         style: TextStyle(
-           fontSize: 24.0,
-           fontWeight: FontWeight.bold,
-         ),),
-        onPressed: () => buttonPressed(buttonText),
-              splashColor: Colors.orange,
-     )
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {//アプリのUIデザインを担当する
-    return Scaffold(
-      appBar: AppBar(//
-        title: Text(widget.title),
-      ),
-      body: new Container(
-        child: new Column(
-          children: <Widget>[
-          new Container(
-            alignment: Alignment.centerRight,
-            padding: new EdgeInsets.symmetric (
-              vertical: 24.0,
-              horizontal: 12.0,
-            ),
-            child: new Text(output, style: new TextStyle(
-              fontSize: 48.0,
-              fontWeight: FontWeight.bold,
-            ),)),
-
-          new Expanded(
-            child: new Divider()
-            ),
-          new Column(children: [
-          new Row(
+Widget build(BuildContext buildContext){
+  return new Scaffold(
+    appBar:null,//appbarは必要ないのでnullしている
+    backgroundColor:Colors.black,
+    body:Container(
+      margin:EdgeInsets.only( bottom:20.0),//余白
+      child: new Column(//縦にWidgetを並べていく、ColumnとRowは入れ子構造にすることができる
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(children: <Widget>[
+            Expanded(
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize:60.0,
+                ),
+                textAlign: TextAlign.right,
+                maxLines: 1,
+              ),
+            )
+          ],
+          ),
+          Row (
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              buildBoutton("7"),
-              buildBoutton("8"),
-              buildBoutton("9"),
-              buildBoutton("/"),
-              ] ),
-          new Row(
-            children: <Widget>[
-              buildBoutton("4"),
-              buildBoutton("5"),
-              buildBoutton("6"),
-              buildBoutton("X"),
-              ] ),
-        new Row(
-            children: <Widget>[
-              buildBoutton("1"),
-              buildBoutton("2"),
-              buildBoutton("3"),
-              buildBoutton("-"),
-              ] ),
-              new Row(
-            children: <Widget>[
-              buildBoutton("."),
-              buildBoutton("0"),
-              buildBoutton("00"),
-              buildBoutton("+"),
-              ] ),
-              new Row(
-            children: <Widget>[
-              buildBoutton("clear"),
-              buildBoutton("="),
-              ] ),
-          ])
-          ]
+              btn('C',const Color(0xffa5a5a5)), // AARRGGBB
+              btn('+/-',const Color(0xffa5a5a5)),
+              btn('%',const Color(0xffa5a5a5)),
+              btn('/', Colors.orange),
+            ],),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+               btn('7',const Color(0xff333333)),
+              btn('8',const Color(0xff333333)),
+              btn('9',const Color(0xff333333)),
+              btn('X',Colors.orange),
+              ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:<Widget>[
+                btn('4',const Color(0xff333333)),
+              btn('5',const Color(0xff333333)),
+              btn('6',const Color(0xff333333)),
+              btn('-',Colors.orange),
+                ],),
+                Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+              btn('1',const Color(0xff333333)),
+              btn('2',const Color(0xff333333)),
+              btn('3',const Color(0xff333333)),
+              btn('+',Colors.orange),
+            ],),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+              btnZero('0',const Color(0xff333333)),
+              btn('.',const Color(0xff333333)),
+              btn('=',Colors.orange),
+            ],),
+        ],
       )
-      ),
+    )
+  );
+}
 
-    );
+void calculation(btnText){
+  if(btnText == 'C'){
+    text = '0';
+    num1 = 0;
+    num2 = 0;
+    result = '';
+    finalresult = '';
+    opr = '';
+    preopr = '';
+
+  }else if(opr == '=' && btnText == '='){
+    if(preopr == '+'){
+      finalresult = add();
+    }else if(preopr == '-'){
+      finalresult = sub();
+    }else if ( preopr == 'X'){
+      finalresult = mul();
+    }else if(preopr == '/'){
+      finalresult = div();
+    }
+
+  }else if(btnText == '+' || btnText == '-' || btnText == 'X' || btnText == '/' || btnText == '='){
+
+    if(num1 == 0){
+      num1 = double.parse(result);
+    }else{
+      num2 = double.parse(result);
+    }
+
+
+    if(opr == '+') {
+          finalresult = add();
+      } else if( opr == '-') {
+          finalresult = sub();
+      } else if( opr == 'X') {
+          finalresult = mul();
+      } else if( opr == '/') {
+          finalresult = div();
+      }
+      preopr = opr;
+      opr = btnText;
+      result = '';
+
   }
+else if(btnText == '%'){
+  result = (num1/100) as String;
+  finalresult = doesContainDecimal(result);
+}else if(btnText == '.'){
+  if(!result.toString().contains('.')){
+    result = result.toString()+ '.';
+  }
+  finalresult = result;
+}
+else if(btnText == '+/-'){
+  result.toString().startsWith('-') ? result = result.toString().substring(1):result = '-' + result.toString();
+  finalresult = result;
+}
+else {
+  result = result + btnText;
+  finalresult = result;
+}
+
+setState(() {
+  text = finalresult;
+});
+}
+
+String add(){
+  result = (num1 + num2).toString();
+  num1 = double.parse(result);
+  return doesContainDecimal(result);
+}
+
+String sub() {
+         result = (num1 - num2).toString();
+          num1 = double.parse(result);
+         return doesContainDecimal(result);
+  }
+
+   String mul() {
+         result = (num1 * num2).toString();
+          num1 = double.parse(result);
+         return doesContainDecimal(result);
+  } String div() {
+          result = (num1 / num2).toString();
+           num1 = double.parse(result);
+          return doesContainDecimal(result);
+  }
+
+  String doesContainDecimal(dynamic result) {
+
+    if(result.toString().contains('.')) {
+        List<String> splitDecimal = result.toString().split('.');
+        if(!(int.parse(splitDecimal[1]) > 0))
+         return result = splitDecimal[0].toString();
+    }
+    return result;
+  }
+
+
+
+
 }
 
